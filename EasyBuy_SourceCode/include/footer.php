add4f1a602c3fec723196cba50e1944e2c445582
mahfujurr221 <86224885+mahfujurr221@users.noreply.github.com>
2021-09-21 12:28:37 +0600

    Add files via upload

---
+++ EasyBuy_SourceCode/include/footer.php
@@ -0,0 +1,85 @@
+<div id="footer" class="mt-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <h4>Pages</h4>
+                <ul>
+                    <li><a href="cart.php">Shopping cart</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="cheakout.php">My Account</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                </ul>
+                <hr>
+                <h4>User Section</h4>
+                <ul>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="customer_register.php">Register</a></li>
+                </ul>
+            </div>
+            
+            <div class="col-md-3 col-sm-6">
+                <h4>Top Product Categories</h4>
+                <ul>
+                    <li><a href="#">Watch</a></li>
+                    <li><a href="#">T-shirt</a></li>
+                    <li><a href="#">Headphones</a></li>
+                    <li><a href="#">keyboard</a></li>
+                </ul>
+            </div>
+            
+            
+            <div class="col-md-3 col-sm-6">
+                <h4>Find Us</h4>
+                <strong>Easybuy media</strong>
+                <p>Shongkor, dhanmondi 1207</p>
+                <span>Email: </span><a href="gmail.com">mahfujurr221@gmail.com</a>
+                <p>Phone:+8801781342259</p>
+                
+                <strong>For more,</strong>
+                <a href="contact.php">Go to contact page</a>
+            </div>
+            
+            
+            <div class="col-md-3 col-sm-6">
+                <h4>Get The News</h4>
+                <p>Dont miss our lastest update</p>
+                
+                <form action="" method="post">
+                    <div class="input-group ">
+                        <input type="text" class="form-control" name="email">
+                        <span class="input-group-btn">
+                            <input type="submit" value="subscribe" class="btn btn-default">
+                        </span>
+                    </div>
+                </form>
+                
+                <hr class="hidden-sm hidden-md hidden-lg">
+                
+                <h4>Keep in touch</h4>
+                
+                <p class="social">
+                    <a href="www.facebook.com" class="fab fa-facebook"></a>
+                    <a href="www.twiter.com" class="fab fa-twitter"></a>
+                    <a href="www.instagram.com" class="fab fa-instagram"></a>
+                    <a href="www.google.com" class="fab fa-google-plus"></a>
+                </p>
+
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div id="copyright">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6">
+                <p class="pull-left">&copy; 2020 EasyBuY Store</p>
+            </div>
+            <div class="col-md-6 col-sm-6">
+                <p class="pull-right">-by Limon Mahfuz</p>
+            </div>
+        </div>
+    </div>
+</div>
